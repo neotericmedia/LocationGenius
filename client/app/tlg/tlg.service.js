1dd04aef54d63e9273c322a7c848384f36f956e4
Doug Garrigan <doug@neotericmediainc.com>
2015-09-04 09:52:33 -0400

    updates to services

--- client/app/tlg/tlg.service.js
+++ client/app/tlg/tlg.service.js
@@ -30,72 +30,31 @@ angular.module('telusLg2App')
   })
 
 
-
-
-
-
-  .service('NetworkDataA', function ($resource) {
-      return $resource('http://192.99.16.178:9105/api/v1/carrierreport/180/5?endDate=2014-08-19', {}, {
-          query: {method: 'GET', isArray:true, },
-      });
-  })
-
-
-
-
-
+  // .service('NetworkDataA', function ($resource) {
+  //     return $resource('http://192.99.16.178:9105/api/v1/carrierreport/180/5?endDate=2014-08-19', {}, {
+  //         query: {method: 'GET', isArray:true, },
+  //     });
+  // })
 
   //ISSUE
   //Request header field Authorization is not allowed by Access-Control-Allow-Headers
   .service('TweeterSearch', function ($resource) {
       return $resource('http://54.85.105.154:7777/tweeters?meters=:range&days=:days&top=:top&address=:searchString', {}, {
-      //return $resource('http://52.3.87.216:9100/twitter/tweeters?meters=:range&days=:days&top=:top&address=:searchString', {}, {
           query: {method: 'GET', isArray:true, },
       });
   })
 
-
-
-
-
-
-
-  .service('TweeterResults', function () {
-    var tweeters;
-    return{
-        getTweeters: function () {
-            return tweeters;
-        },
-        setTweeters: function (results) {
-            tweeters = results;
-        }
-    }
-  })
-
-
-
-
-
-  // .factory('NetworkReports', function ($resource) {
-  //    return $resource('data/outputsNetwork2.json', {})
-  // })
-
-
-
-
-
-  // talks to Backend carrier data API route
-  .service('CarrierPregenReport', function ($resource) {
-     return $resource('/api/carrier', {}, {
-         query: {method: 'GET', isArray:true},
-     });
-  })
-
-
-
-
-
-
+   .service('TweeterResults', function () {
+       var tweeters;
+       return{
+           getTweeters: function () {
+               return tweeters;
+           },
+           setTweeters: function (results) {
+               tweeters = results;
+           }
+       }
+   })
 
    .service('TweetReports', function ($resource) {
       return $resource('http://54.86.239.240:7777/twitterdayreports/:locationId?days=:days&top=100', {}, {
@@ -103,12 +62,7 @@ angular.module('telusLg2App')
       });
    })
 
-
-
    .service('OnsitePregenReport', function ($resource) {
-      //return $resource('http://54.85.105.154:80/onsitereport/:buildingId?date=:dt', {}, {
-    //api/onsite/180?date=2015-04-26
-      console.log( "Getting /api/onsite/"  );
       return $resource('/api/onsite/:buildingId?date=:dt', {}, {
           query: {
              method: 'GET',
@@ -117,20 +71,8 @@ angular.module('telusLg2App')
       });
    })
 
-
-// talks to Backend carrier data API route
   .service('CarrierPregenReport', function ($resource) {
-    //var params = {"locationId": $scope.currentLocation.buildingId, "days":numDays, "endDate":"2014-08-18"};
     return $resource('/api/carrier/:locationId/:days?endDate=:endDate', {}, {
       query: {method: 'GET', isArray:true},
     });
   })
-
-
-
-
-
-
-
-
-
