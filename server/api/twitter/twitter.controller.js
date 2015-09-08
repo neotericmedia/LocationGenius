969da7e22a8ea6f7febd369639432c27b609e512
david <dcamp200@gmail.com>
2015-09-08 11:30:19 -0400

    Changed Tweets to point to api gateway

--- server/api/twitter/twitter.controller.js
+++ server/api/twitter/twitter.controller.js
@@ -5,32 +5,9 @@ var request = require('request');
 
 
 
-// Get a twitter day report
-// e.g.192.99.16.178:9100/twitter/dayreports/180?days=7&top=100
-exports.show = function(req, res) {
-
-  console.log('Getting twitter day report...' + req.query.days + " " + req.query.top);
-  request({
-    url: 'http://192.99.16.178:9100/twitter/dayreports/' + req.params.id + '?days=' + req.query.days + '&top=' + req.query.top,
-    method: 'GET',
-    headers: {
-      'Authorization': 'Basic bGd3ZWI6bGdlbjF1cw=='
-    }
-  }, function(error, response, body){
-    if(error) {
-      console.log(error);
-    } else {
-      //console.log("Twitter Response:" + body);
-      res.send(body)
-    }
-
-  });
-};
-
-
 // Get tweets
 // e.g.192.99.16.178:9100/twitter/dayreports/180?days=7&top=100
-exports.tweeters = function(req, res) {
+exports.show = function(req, res) {
 
   console.log('Getting tweeters...' + req.query.days + " " + req.query.top + " " + req.query.meters + " " + req.query.address);
   request({
@@ -43,7 +20,7 @@ exports.tweeters = function(req, res) {
     if(error) {
       console.log(error);
     } else {
-      //console.log("Twitter Response:" + body);
+      console.log("Twitter Response:" + body);
       res.send(body)
     }
 
